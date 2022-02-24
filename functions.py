41a50fcc952901fad1301ca8d1ea9343ac92e16d
Bekono Roland <bekonrolang@gmail.com>
2022-02-24 09:18:00 +0100

    Add addpassager,isplaceavaible functions

--- functions.py
+++ functions.py
@@ -28,6 +28,7 @@ def generateId(number,type):
 
 from os import system, name
 from time import sleep
+from unittest import result
 def clear():
    # for windows
    if name == 'nt':
@@ -39,3 +40,44 @@ def clear():
 #print("Hi Learner!!")
 #sleep(5)
 #clear()
+def addPassagerTobus(passager,bus):
+    bus["passagers"].append(passager)
+def isPlaceAvailable(bus):
+    result=False
+    if bus["nombrePlace"]>len(bus["passagers"]):
+        result=True
+    return result
+def isPoidsOverFlow(passager,bus) :
+    result=False
+    summe=0
+    for x in bus["passagers"]:
+        summe+=x["poidsBaggage"]
+    if (bus["poidsMax"]<(summe+passager["poidsBaggage"])):
+        result=True
+    return result
+def numberOfAvailablePlace(bus):
+    if bus["nombrePlace"]>len(bus["passagers"]):
+        return (bus["nombrePlace"]-len(bus["passagers"]))
+    else:
+        return 0
+def numberOfWeigthAvailable(bus):
+    summe=0
+    for x in bus["passagers"]:
+        summe+=x["poidsBaggage"]
+    if (bus["poidsMax"]>(summe)):
+        return bus["poidsMax"]-(summe)
+    else:
+        return 0
+def removePassagerInBus(passager,bus):
+    if(isPassagerIntoBus(passager,bus)):
+        bus["passagers"].remove(passager)
+        print("Passager {} removed from bus {} ".format(passager["Id"],bus["Id"]))
+    else:
+        print("Passager doesnt exist on bus")
+def isPassagerIntoBus(passager,bus):
+    result=False
+    for x in bus["passagers"]:
+        if x["Id"]==passager["Id"]:
+            result=True
+            break
+    return result
