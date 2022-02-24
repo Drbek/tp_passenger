c1fb4ac35e5ebed76c2bc97f7c96cb02ccd5e648
Bekono Roland <bekonrolang@gmail.com>
2022-02-24 07:39:04 +0100

    add getBus and getListBus functions

---
+++ create-bus.py
@@ -0,0 +1,42 @@
+import copy
+from functions import getValidNumber
+from functions import generateId
+from functions import clear
+modelBus={
+    "matricule":None,
+    "nombrePlace":0,
+    "poidsMax":0,
+    "passagers":[]
+}
+def getBus(lastIndex):
+    print("-----------------------------------------------------------")
+    print("----------------AJOUTER UN NOUVEAU BUS---------------------")
+    print("-----------------------------------------------------------")
+    nombrePlace=getValidNumber("Quel est le nombre de place ? : ")
+    poidsMax=getValidNumber("Quel est le poids max du bus ? : ")
+    bus=copy.deepcopy(modelBus)
+    bus["nombrePlace"]=nombrePlace
+    bus["poidsMax"]=poidsMax
+    bus["matricule"]=generateId(lastIndex,"BUS-")
+    return bus
+#test
+#passa=getPassager(0)
+#clear()
+#print(passa)
+def getListBus():
+    listBus=[]
+    continueRegister=True
+    while continueRegister :
+        bus=getBus(len(listBus))
+        listBus.append(bus)
+        confirm=input("Voulez ajouter un nouveau bus ? (oui/non) : ")
+        if(confirm=="non") :
+            continueRegister=False
+        clear()
+    return listBus
+#test
+listP=getListBus()
+
+
+
+
