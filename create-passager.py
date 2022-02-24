24e1ad482145896a7aa91b06927fcc304c8665f4
Bekono Roland <bekonrolang@gmail.com>
2022-02-24 06:48:42 +0100

    import util function to create-passager.py

--- create-passager.py
+++ create-passager.py
@@ -1,15 +1,23 @@
 import copy
+from functions import getValidNumber
+from functions import generateId
 modelPassager={
     "Id":None,
     "nom":None,
     "prenom":None,
     "poidsBaggage":None
 }
-def getPassager():
+def getPassager(lastIndex):
     nom=input("Nom du passager : ")
     prenom=input("Prenom du passager : ")
-    poidsBaggage=getValidNumber(input("Poids des baggages : "))
+    poidsBaggage=getValidNumber("Poids des baggages : ")
     passager=copy.deepcopy(modelPassager)
     passager["nom"]=nom
     passager["prenom"]=prenom
     passager["poidsBaggage"]=poidsBaggage
+    passager["Id"]=generateId(lastIndex,"PA")
+    return passager
+#test
+#passa=getPassager(0)
+#print(passa)
+
